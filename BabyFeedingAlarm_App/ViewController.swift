46fcc3eb8b32110fad1a645aba54e42eadb22754
Ingyu Woo <iglenwoo@gmail.com>
2019-01-13 14:55:11 -0800

    create xcode project

---
+++ BabyFeedingAlarm_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BabyFeedingAlarm_App
+//
+//  Created by Ingyu Woo on 1/13/19.
+//  Copyright © 2019 Ingyu Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

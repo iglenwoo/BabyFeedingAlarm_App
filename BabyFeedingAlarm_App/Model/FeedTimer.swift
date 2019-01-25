88e2a0b2440322d7b3374f499bf4b5bf79a12dc8
Ingyu Woo <iglenwoo@gmail.com>
2019-01-25 15:41:22 -0800

    enhance timer performance

--- BabyFeedingAlarm_App/Model/FeedTimer.swift
+++ BabyFeedingAlarm_App/Model/FeedTimer.swift
@@ -12,7 +12,7 @@ class FeedTimer {
 
     //MARK: Properties
 
-    let updateInterval: Double = 0.01
+    let updateInterval: Double = 0.1
     var (hours, minutes, seconds, fractions) = (0, 0, 0, 0)
     var status = Status.stop
     var timer: Timer?
@@ -40,13 +40,17 @@ extension FeedTimer {
     func start() {
         status = .start
 
+        // create the timer object without scheduling it on a run loop
         self.timer = Timer.init(timeInterval: updateInterval, target: self, selector: #selector(self.updateTime), userInfo: nil, repeats: true)
+        // The timer may fire at any time between its scheduled fire date and the scheduled fire date plus the tolerance.
+        // A general rule, set the tolerance to at least 10% of the interval, for a repeating timer.
+        timer?.tolerance = updateInterval * 0.2 //20%
         RunLoop.current.add(timer!, forMode: .common)
     }
 
     @objc func updateTime() {
         fractions += 1
-        if (fractions > 99) {
+        if (fractions > 9) {
             seconds += 1
             fractions = 0
         }

ce003e979e1d0205b5d0e01f4a9cd76c93943c6d
Ingyu Woo <iglenwoo@gmail.com>
2019-03-08 15:50:00 -0800

    add test notification func

--- BabyFeedingAlarm_API/functions/index.js
+++ BabyFeedingAlarm_API/functions/index.js
@@ -1,21 +1,62 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
-
-exports.printOut = functions.database.ref('/started/{uid}').onCreate((snapshot, context) => {
-  const val = snapshot.val();
-  if (!val) {
-    console.log(`val is ${val}`)
-  }
-  const uid = context.params.uid;
-  if (!uid) {
-    console.log(`uid is ${uid}`)
-  }
-
-  console.log(`Started ! (val: ${val} | uid: ${uid})`);
+var serviceAccount = require('./private/key.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://babyfeedingalarm.firebaseio.com"
 });
 
-function getUsersOverTime() {
-  const tenMinBefore = Date.now()
-  functions.database.ref('/started').orderByChild('initialTime')
-}
+const gap = {
+  '10sec': 10000,
+  '30sec': 30000,
+  '5min': 300000,
+  '10min': 600000
+};
+var payload = {
+  notification: {
+    title: 'This is title',
+    body: 'Test from Functions'
+  },
+  data: { }
+};
+
+exports.notifyOverTime = functions.https.onRequest((req, res) => {
+  const currentTime = new Date().getTime();
+  const overTime = currentTime - gap["30sec"];
+  const fcmTokens = [];
+
+  admin.database().ref('started').orderByChild('initialTime').startAt(overTime).once('value')
+      .then(snap => {
+        snap.forEach(childSnap => {
+          const token = childSnap.val().fcmToken;
+          fcmTokens.push(token)
+        });
+        console.log(`Tokens: ${fcmTokens}`);
+
+        return fcmTokens;
+      })
+      .then(tokens => {
+        tokens.forEach(token => {
+          // payload.body = `body: ${token}`;
+          console.log(`sent a noti to ${token} with\n${payload.body}`);
+
+          admin.messaging().sendToDevice(token, payload)
+          // .then(response => {
+          //   console.log('Successfully sent message:', response);
+          //   return;
+          // })
+          // .catch(error => {
+          //   console.log('Error sending message:', error);
+          // })
+        });
+
+        res.send(JSON.stringify(fcmTokens));
+
+        return;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    // res.send(JSON.stringify(fcmTokens));
+});

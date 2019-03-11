f11fa3a9bb2f2697cf3291a88d6ee3f1194cb7dd
Ingyu Woo <iglenwoo@gmail.com>
2019-03-10 22:32:14 -0700

    use seconds for times

--- BabyFeedingAlarm_API/functions/index.js
+++ BabyFeedingAlarm_API/functions/index.js
@@ -7,56 +7,57 @@ admin.initializeApp({
 });
 
 const gap = {
-  '10sec': 10000,
-  '30sec': 30000,
-  '5min': 300000,
-  '10min': 600000
+  '10sec': 10,
+  '30sec': 30,
+  '5min': 300,
+  '20min': 1200
 };
 var payload = {
   notification: {
-    title: 'This is title',
-    body: 'Test from Functions'
+    title: 'Stop Feeding',
+    body: 'Your are over 20 min.'
   },
   data: { }
 };
 
 exports.notifyOverTime = functions.https.onRequest((req, res) => {
-  const currentTime = new Date().getTime();
-  const overTime = currentTime - gap["30sec"];
+  const currentTime = new Date().getTime() / 1000; // make it seconds, not ms.
+  console.log(`currentTime: ${currentTime}`);
   const fcmTokens = [];
 
-  admin.database().ref('started').orderByChild('initialTime').startAt(overTime).once('value')
-      .then(snap => {
-        snap.forEach(childSnap => {
-          const token = childSnap.val().fcmToken;
-          fcmTokens.push(token)
-        });
-        console.log(`Tokens: ${fcmTokens}`);
-
-        return fcmTokens;
-      })
-      .then(tokens => {
-        tokens.forEach(token => {
-          // payload.body = `body: ${token}`;
-          console.log(`sent a noti to ${token} with\n${payload.body}`);
-
-          admin.messaging().sendToDevice(token, payload)
-          // .then(response => {
-          //   console.log('Successfully sent message:', response);
-          //   return;
-          // })
-          // .catch(error => {
-          //   console.log('Error sending message:', error);
-          // })
-        });
-
-        res.send(JSON.stringify(fcmTokens));
-
-        return;
-      })
-      .catch(error => {
-        console.log(error);
+  admin.database().ref('started').orderByChild('initialTime').once('value')
+    .then(snap => {
+      snap.forEach(childSnap => {
+        console.log(childSnap.val());
+        const token = childSnap.val().fcmToken;
+        fcmTokens.push(token)
       });
+
+      return fcmTokens;
+    })
+    .then(tokens => {
+      tokens.forEach(token => {
+        notifyDevices(token, payload);
+      });
+
+      res.send(JSON.stringify(fcmTokens));
+
+      return;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
     // res.send(JSON.stringify(fcmTokens));
 });
+
+const notifyDevices = (token, payload) => {
+  admin.messaging().sendToDevice(token, payload)
+    // .then(response => {
+    //     console.log('Successfully sent message:', response);
+    //     return;
+    // })
+    .catch(error => {
+        console.log('Error sending message:', error);
+    });
+};

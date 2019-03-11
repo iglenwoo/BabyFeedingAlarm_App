a28ba7f235e3fc2a58c30c7cdf377cfcc183a9eb
Ingyu Woo <iglenwoo@gmail.com>
2019-03-11 13:44:27 -0700

    send noti (dev: gap is only 10 sec)

--- BabyFeedingAlarm_API/functions/index.js
+++ BabyFeedingAlarm_API/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const secureCompare = require('secure-compare');
 var serviceAccount = require('./private/key.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -12,44 +13,62 @@ const gap = {
   '5min': 300,
   '20min': 1200
 };
+const timeGap = gap["10sec"];
+// const timeGap = gap["20min"];
+
 var payload = {
   notification: {
-    title: 'Stop Feeding',
+    title: 'BabyFeedingAlarm',
     body: 'Your are over 20 min.'
   },
   data: { }
 };
 
 exports.notifyOverTime = functions.https.onRequest((req, res) => {
-  const currentTime = new Date().getTime() / 1000; // make it seconds, not ms.
-  console.log(`currentTime: ${currentTime}`);
+  const key = req.query.key;
+
+  if (!secureCompare(key, functions.config().cron.key)) {
+    console.log('The key provided in the request does not match the key set in the environment. Check that', key,
+        'matches the cron.key attribute in `firebase env:get`');
+    res.status(403).send('Security key does not match. Make sure your "key" URL query parameter matches the ' +
+        'cron.key environment variable.');
+    return null;
+  }
+
+  const currentTime = getCurrentTime();
   const fcmTokens = [];
 
   admin.database().ref('started').orderByChild('initialTime').once('value')
     .then(snap => {
       snap.forEach(childSnap => {
-        console.log(childSnap.val());
-        const token = childSnap.val().fcmToken;
-        fcmTokens.push(token)
+        const initialTimeStr = childSnap.val().initialTime;
+        const initialTime = Number(initialTimeStr);
+        const timeAfterGap = initialTime + timeGap;
+
+        if (timeAfterGap <= currentTime) {
+          if (! childSnap.val().isNotified) {
+            const token = childSnap.val().fcmToken;
+            fcmTokens.push(token);
+            notifyDevices(token, payload);
+            updateNotified(childSnap.key, currentTime);
+          }
+        }
+
       });
 
-      return fcmTokens;
-    })
-    .then(tokens => {
-      tokens.forEach(token => {
-        notifyDevices(token, payload);
-      });
-
-      res.send(JSON.stringify(fcmTokens));
-
-      return;
+      return null;
     })
     .catch(error => {
       console.log(error);
     });
 
-    // res.send(JSON.stringify(fcmTokens));
+    res.send();
+    return null;
 });
+
+const getCurrentTime = () => {
+  return Date.now() / 1000 // make it seconds, not ms.
+};
 
 const notifyDevices = (token, payload) => {
   admin.messaging().sendToDevice(token, payload)
@@ -60,4 +79,14 @@ const notifyDevices = (token, payload) => {
     .catch(error => {
         console.log('Error sending message:', error);
     });
+};
+
+const updateNotified = (key, currentTime) => {
+  const db = admin.database();
+  const ref = db.ref("started");
+  const timeRef = ref.child(key);
+  timeRef.update({
+    'isNotified': true,
+    'notifiedTime': currentTime
+  });
 };
